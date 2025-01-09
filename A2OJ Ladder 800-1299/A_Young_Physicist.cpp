7598a114324069e9956c77cb87c57b52553a98fe
Hemant Raj <coder.hemantraj05@gmail.com>
2025-01-10 01:17:46 +0530

    Young physicist solved

--- A2OJ Ladder 800-1299/A_Young_Physicist.cpp
+++ A2OJ Ladder 800-1299/A_Young_Physicist.cpp
@@ -4,11 +4,30 @@
 using namespace std;
 
 int main(){
-    int TC;
-    cin >> TC;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    while(TC--){
-        
+    ll n;
+    cin >> n;
+
+    vector<vector<ll>>forces(n,vector<ll>(3));
+    ll x=0,y=0,z =0;
+    for(ll i = 0; i < n; i++){
+        for(ll j = 0; j < 3; j++){
+            cin >> forces[i][j];
+        }
+        x += forces[i][0];
+        y += forces[i][1];
+        z += forces[i][2];
+    }
+
+
+
+    if( x!= 0 || y != 0 || z != 0){
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
     }
 
     return 0;
